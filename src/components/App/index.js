4f4b26773e7819f79931ee7e98f336617bda8ed0
baptaste <babachampbenoit@gmail.com>
2021-07-12 17:29:51 +0200

    Ajout datas dynamiques

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,12 +6,13 @@ import './app.scss';
 import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
 import Footer from 'src/components/Footer';
+import data from 'src/data/currencies';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
-    <Currencies />
+    <Currencies currencies={data}/>
     <Footer />
   </div>
 );
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,29 +1,13 @@
 import React from 'react';
+
 import './currencies.scss';
 
-const Currencies = () => (
+const Currencies = ({ currencies }) => (
   <ul className="currencies__list">
     <li className="currencies__list__title">Currencies</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
-    <li className="currencies__list__currency">Test</li>
+    {currencies.map((currency) => (
+      <li key={currency.name} className="currencies__list__currency">{currency.name}</li> 
+    ))};
   </ul>
 );
 
